0557bc3e150ea49535af6dd6854c2fd8facfb0a4
pc-PC\pc <905304437@qq.com>
2016-09-20 12:35:53 +0800

    Finish Homework 2 2016.9.20

--- pyramid.ts
+++ pyramid.ts
@@ -1,15 +1,15 @@
 function out(m : number) : void
 {
-    for(var i : number = 1; i <= m ; i++)
+    for(var i : number = 1 ; i <= m ; i++)
     {
         var a : string = "";
-        for(var j : number = 1;j <= m - i;j++)
+        for(var j : number = 1 ; j <= m - i ; j++)
         {
             var space : string = " ";
             a = a + space;
         }
         
-        for(var k : number = 1;k <= 2*i - 1;k++)
+        for(var k : number = 1 ; k <= 2*i - 1 ; k++)
         {
             var star : string = "*";
             a = a + star;
